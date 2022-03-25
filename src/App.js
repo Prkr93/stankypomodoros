0527779ea7965d80368bc44abfd5fe65fc783366
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-24 19:53:19 -0600

    Import test data from test file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import './css/style.css';
-import Header from './header'
-import MainContent from './mainContent'
-import Nav from './nav'
-import Footer from './footer'
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div className='app'>
-      <Header />
-      <MainContent />
-      <Nav />
-      <Footer />
-      <h1>Hello!!!</h1>
-    </div>
-  );
+import './css/style.css';
+import Header from './header';
+import MainContent from './mainContent';
+import Nav from './nav';
+import Footer from './footer';
+import movieData from './data/test-data';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+    }
+  }
+  render() {
+    return (
+      <div className='app'>
+        <Header />
+        <MainContent />
+        <Nav />
+        <Footer />
+        <h1>Hello!!!</h1>
+      </div>
+    )
+  }
+
 }
 
 export default App;
