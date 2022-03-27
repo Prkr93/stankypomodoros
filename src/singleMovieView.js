5a47e2762a69ad92b42b62bfa9478d0a18e1a6e1
Dillon Parker <prkr93@gmail.com>
2022-03-27 12:37:10 -0600

    Add responsive styles for singleMovieView

--- src/singleMovieView.js
+++ src/singleMovieView.js
@@ -12,52 +12,51 @@ const SingleMovieView = ({selectedMovie, toggleHighlighted}) => {
       <section className='backdrop' style={style}>
         <h2>{ selectedMovie.title }</h2>
       </section>
-      <section className='single-movie-info'>
         {selectedMovie.overview &&
-          <div>
+          <div className='tagline'>
             <p>{ selectedMovie.tagline }</p>
           </div>
         }
+      <section className='single-movie-info'>
         <img src={selectedMovie.poster_path}/>
         {selectedMovie.overview &&
-          <div>
+          <div className='overview'>
             <h3>Overview:</h3>
             <p>{ selectedMovie.overview }</p>
           </div>
         }
         {selectedMovie.budget &&
-          <div>
+          <div className='budget'>
             <h4>Budget: </h4>
             <p>{ selectedMovie.budget }</p>
           </div>
         }
         {selectedMovie.average_rating &&
-          <div>
+          <div className='average_rating'>
             <h4>Rating: </h4>
             <p>{ selectedMovie.average_rating }</p>
           </div>
         }
         {selectedMovie.runtime &&
-          <div>
+          <div className='runtime'>
             <h4>Runtime: </h4>
             <p>{ selectedMovie.runtime }</p>
           </div>
         }
         {selectedMovie.revenue &&
-          <div>
+          <div className='revenue'>
             <h4>Revenue: </h4>
             <p>{ selectedMovie.revenue }</p>
           </div>
         }
         {selectedMovie.release_date &&
-          <div>
+          <div className='overrelease_dateview'>
             <h4>Release Date: </h4>
             <p>{ selectedMovie.release_date }</p>
           </div>
         }
-
         {selectedMovie.genres &&
-          <div>
+          <div className='genres'>
             <h4>Genres: </h4>
             <p>{ selectedMovie.genres }</p>
           </div>
