1246dc4fbfa3bf07474cde3abc9182dc5359bd88
Dillon Parker <prkr93@gmail.com>
2022-03-30 17:58:03 -0600

    Add content and styling for dashboard slideshow

--- src/slideshow.js
+++ src/slideshow.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import topRated from './movieList';
 
-const Slideshow = () => {
+const Slideshow = ({topRated}) => {
+
+  let topMovies = topRated.map((movie, index) => {
+    let style = {
+      backgroundImage: `url(${movie.backdrop_path})`
+    }
+    return (
+      <article className={(index === 0 ? 'active' : '')} style={style} key={movie.id}>
+        <h2>{movie.title}</h2>
+        <button>View Info</button>
+      </article>
+    )
+  });
 
   return (
-    <section> slideshow bob </section>
+    <section className='slideshow'>
+      <section className='slider'>{topMovies}</section>
+      <section className='ticker'>
+        <div className='tick-one active'></div>
+        <div className='tick-two'></div>
+        <div className='tick-three'></div>
+        <div className='tick-four'></div>
+        <div className='tick-five'></div>
+      </section>
+    </section>
   )
 }
 
