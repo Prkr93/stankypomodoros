b0850fa3b6b202ad0f8bb2fab1069843fc4ae9d7
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-30 19:37:48 -0600

    Install react player, fetch video data and write js functionality to play trailers on single movie view

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Nav from './nav';
 import Footer from './footer';
 import SingleMovieView from './singleMovieView';
 import movieData from './data/test-data';
-import {movieDatabase, singleMovieData, passData} from './apiCalls'
+import {movieDatabase, singleMovieData, passData, singleVideoData} from './apiCalls'
 import {Route, NavLink} from 'react-router-dom';
 
 
@@ -18,16 +18,19 @@ class App extends Component {
       movies: [],
       filterOption: 'bangersToStankers',
       selectedMovie: false,
+      selectedVideos: false,
     }
   }
 
   fetchData() {
-    Promise.all([movieDatabase, singleMovieData]).then(data => {
+    Promise.all([movieDatabase, singleMovieData, singleVideoData]).then(data => {
        this.setState({ movies: data[0].movies });
-       (data[1] && this.getHighlighted(data[1]))
+       // (data[1] && this.getHighlighted(data[1]))
+       (data[1] && this.setState({ selectedMovie: data[1].movie }));
+       (data[2] && this.getHighlightedVideo(data[2]))
+
     })
   }
-
   componentDidMount() {
     const moviePath = window.location.href;
     const movieId = moviePath.slice(moviePath.length -6)
@@ -39,13 +42,18 @@ class App extends Component {
   }
 
   getHighlighted(singleData) {
-    this.setState({ selectedMovie: singleData.movie });
+
+  }
+  getHighlightedVideo(videoData) {
+    let videoKeys = videoData.videos.map(video => video.key)
+    let videos = `https://www.youtube.com/watch?v=${videoKeys[0]}`
+    this.setState({ selectedVideos: videos });
   }
 
   toggleHighlighted = (e) => {
 
     if (this.state.selectedMovie) {
-      this.setState({selectedMovie: false})
+      this.setState({selectedMovie: false, selectedVideos: false})
     } else {
 
       let highlightedMovie = this.state.movies.find(movie => movie.id == e.target.id);
@@ -57,7 +65,9 @@ class App extends Component {
   render() {
     return (
       <div className='app' >
-        <Header />
+        <Header
+        toggleHighlighted={this.toggleHighlighted}
+        />
         <Route exact path='/' render={() =>
           <MainContent
             movieRepo={this.state.movies}
@@ -69,6 +79,7 @@ class App extends Component {
           <SingleMovieView
             selectedMovie={this.state.selectedMovie}
             toggleHighlighted={this.toggleHighlighted}
+            selectedVideos={this.state.selectedVideos}
           />
         } />
         <Nav />
