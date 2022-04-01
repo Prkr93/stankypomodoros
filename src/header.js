ad743c87dd6fca8a5a9ad6157d89ee86f893e715
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-31 20:15:04 -0600

    Fix merge conflict

--- src/header.js
+++ src/header.js
@@ -8,7 +8,6 @@ const Header = ({toggleHighlighted}) => {
     <header>
       <h1><NavLink to='/'>Stanky Pomodoros</NavLink></h1>
       <img className='logo' src={logo} />
->>>>>>> e59bf0b3b6f70adf37db071f1a3e8605b8645a70
       <button>🍔</button>
     </header>
   )
