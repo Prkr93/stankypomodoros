052b255451b11d41eae0e62cf9fd0a9676d1404f
Dillon Parker <prkr93@gmail.com>
2022-04-02 14:06:52 -0600

    Fix bug for absTitle and setup dynamic render for movieCards display

--- src/movieList.js
+++ src/movieList.js
@@ -3,12 +3,23 @@ import MovieCard from './movieCard';
 
 const MovieList = ({ movieRepo, toggleHighlighted, filteredMovies }) => {
 
+  //Why is rendering 3 times?
+
+  //Why is filteredMovies, on the third render undefined?
+
   console.log(filteredMovies, '<<filtered')
 
-  const movieCards = movieRepo.map(movie => <MovieCard
-                                              movie={movie}
-                                              key={movie.id}
-                                              toggleHighlighted={toggleHighlighted} />);
+  let repo = filteredMovies ? filteredMovies : movieRepo;
+
+  const movieCards = repo.map(movie => <MovieCard
+                              movie={movie}
+                              key={movie.id}
+                              toggleHighlighted={toggleHighlighted}
+                              />
+                            );
+
+
+
 
   return (
     <section className='movie-list'> {movieCards} </section>
