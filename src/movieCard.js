27eb05e88e560952732af476f05c7dec03a9157f
Dillon Parker <prkr93@gmail.com>
2022-04-02 17:16:22 -0600

    Fix responsive styling for movie cards

--- src/movieCard.js
+++ src/movieCard.js
@@ -3,18 +3,14 @@ import {NavLink} from 'react-router-dom';
 
 const MovieCard = ({ movie, toggleHighlighted }) => {
 
-let style;
-  if (movie.backdrop_path.includes('NoPhotoAvailable')) {
-    style = {
-      backgroundImage: `url('https://i.imgflip.com/6b66bi.jpg')`
-    }
-  } else if (!movie.backdrop_path.includes('NoPhotoAvailable')) {
-    style = {
-      backgroundImage: `url(${movie.backdrop_path})`
-    }
-  }
+  let url;
+  movie.backdrop_path.includes('NoPhotoAvailable') ?
+    url = 'https://i.imgflip.com/6b66bi.jpg' :
+    url = `${movie.backdrop_path}`;
+
   return (
-    <NavLink id={movie.id} className='movie-card' style={style} to={`movie/${movie.id}`} onClick={toggleHighlighted}>
+    <NavLink id={movie.id} className='movie-card' to={`movie/${movie.id}`} onClick={toggleHighlighted}>
+      <img src={url} />
       <h3>{movie.title}</h3>
     </NavLink>
   )
