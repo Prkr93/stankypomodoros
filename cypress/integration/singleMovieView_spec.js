30941a82f4e66f39d3a3782bfacece8a44628da1
Dillon Parker <prkr93@gmail.com>
2022-04-04 13:37:38 -0600

    Add class for poster image and add tests for single movie view

--- cypress/integration/singleMovieView_spec.js
+++ cypress/integration/singleMovieView_spec.js
@@ -1,22 +1,32 @@
 describe('Dashboard', () => {
 
-  it.skip('Should ', () => {
-
+  it('should display a backdrop image with title at the top', () => {
+    cy.visit('http://localhost:3000/movie/581392')
+      .get('.backdrop')
+      .should('have.css', 'background-image')
+      .get('.backdrop')
+      .children('h2')
   });
 
-  it.skip('', () => {
-
-  }); 
-
-  it('should contain movie details grouped underneath', () => {
+  it('should contain a tagline underneath the backdrop', () => {
     cy.visit('http://localhost:3000/movie/581392')
-        .get('.overview')
-        .siblings('.budget')
-        .siblings('.average_rating')
-        .siblings('.runtime')
-        .siblings('.revenue')
-        .siblings('.overrelease_dateview')
-        .siblings('.genres')
+      .get('.backdrop')
+      .siblings('.tagline')
+  });
+
+  it('should contain single movie info underneath tagline', () => {
+    cy.visit('http://localhost:3000/movie/581392')
+        .get('.single-movie-info')
+        .children('.movie-poster')
+        .siblings('.movie-details')
+  });
+
+  it('should display a video, overview, and details within movie-details', () => {
+    cy.visit('http://localhost:3000/movie/581392')
+      .get('.movie-details')
+      .children('.videos')
+      .siblings('.overview')
+      .siblings('.details')
   });
 
   it('should be able to click the X to return to the dashboard view', () => {
@@ -24,17 +34,6 @@ describe('Dashboard', () => {
         .get('.exit-single-movie-view')
         .click()
           .get('.dashboard')
-  });
-
-  it('should display a backdrop image with title at the top', () => {
-    cy.visit('http://localhost:3000/movie/581392')
-      .get('.backdrop')
-      .should('have.css', 'background-image')
-      .get('h2')
-  });
-
-  it.skip('', () => {
-
   });
 
 });
